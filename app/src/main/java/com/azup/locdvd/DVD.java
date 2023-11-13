ca52681a7e00b4d978c57bd0cc7f990f03ea98ea
Alex Zapata <alexzapata1984@gmail.com>
2023-11-13 23:13:09 +0100

    Persistencia de datos. CRUD

--- app/src/main/java/com/azup/locdvd/DVD.java
+++ app/src/main/java/com/azup/locdvd/DVD.java
@@ -1,6 +1,11 @@
 package com.azup.locdvd;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
 
 public class DVD {
 
@@ -16,7 +21,7 @@ public class DVD {
     }
 
     private DVD(Cursor cursor) {
-        id = cursor.getLong(cursor.getColumnIndex("id"));
+        id = cursor.getLong(cursor.getColumnIndexOrThrow("id"));
         titulo = cursor.getString(cursor.getColumnIndex("titulo"));
         anyo = cursor.getInt(cursor.getColumnIndex("anyo"));
         actores =
@@ -64,4 +69,128 @@ public class DVD {
     public void setResumen(String resumen) {
         this.resumen = resumen;
     }
+
+    /**
+     * Método estático que devuelve una lista de todos los DVDs registrados.
+     * @param context
+     * @return
+     */
+    public static ArrayList<DVD> getDVDList(Context context) {
+        ArrayList<DVD> listDVD = new ArrayList<>();
+
+        LocalSQLiteOpenHelper helper = new
+                LocalSQLiteOpenHelper(context);
+
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        Cursor cursor = db.query(true, "DVD", new String[]{"id",
+                        "titulo", "anyo", "actores", "resumen"}, null,
+                null,null,null,"titulo", null  );
+
+        while (cursor.moveToNext()) {
+            listDVD.add(new DVD(cursor));
+        }
+
+        cursor.close();
+        db.close();
+
+        return listDVD;
+    }
+
+    /**
+     * Permite obtener un DVD a partir de su identificador en la base de datos.
+     * @param context
+     * @return
+     */
+    public static DVD getDVD(Context context, long id) {
+        DVD dvd = null;
+        LocalSQLiteOpenHelper helper = new
+                LocalSQLiteOpenHelper(context);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String where ="id = " + String.valueOf(id);
+        Cursor cursor = db.query(true, "DVD", new String[]{"id",
+                        "titulo", "anyo", "actores", "resumen"}, where,
+                null,null,null,"titulo", null  );
+
+        if(cursor.moveToFirst())
+            dvd = new DVD(cursor);
+
+        cursor.close();
+        db.close();
+
+        return dvd;
+    }
+
+    /**
+     * Inserción de datos. Escritura en base de datos de un nuevo DVD
+     * @param context
+     * @return
+     */
+    public void insert(Context context) {
+        ContentValues values = new ContentValues();
+        values.put("titulo",this.titulo);
+        values.put("anyo",this.anyo);
+        if(this.actores !=null) {
+            String listActores = new String();
+            for(int i = 0; i<this.actores.length; i++) {
+                listActores+=this.actores[i];
+                if(i<this.actores.length-1)
+                    listActores+=";";
+            }
+            values.put("actores",listActores);
+        }
+        values.put("resumen",this.resumen);
+
+        LocalSQLiteOpenHelper helper = new
+                LocalSQLiteOpenHelper(context);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        this.id=db.insert("DVD", null, values);
+        db.close();
+    }
+
+    /**
+     * Actualizar un registro de la base de datos de DVD
+     * @param context
+     * @return
+     */
+    public void update(Context context) {
+        ContentValues values = new ContentValues();
+        values.put("titulo",this.titulo);
+        values.put("anyo",this.anyo);
+        if(this.actores !=null) {
+            String listActores = new String();
+            for(int i = 0; i<this.actores.length; i++) {
+                listActores+=this.actores[i];
+                if(i<this.actores.length-1)
+                    listActores+=";";
+            }
+            values.put("actores",listActores);
+        }
+        values.put("resumen",this.resumen);
+        String whereClause = "id=" + String.valueOf(this.id);
+        LocalSQLiteOpenHelper helper = new LocalSQLiteOpenHelper(context);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.update("DVD", values,whereClause,null);
+        db.close();
+    }
+
+    /**
+     * Elimina un registro de la base de datos indicando un id
+     * @param context
+     * @return
+     */
+    public void delete(Context context) {
+        String whereClause = "id= ?" ;
+        String[] whereArgs = new String[1];
+        whereArgs[0] = String.valueOf(this.id);
+        LocalSQLiteOpenHelper helper = new
+                LocalSQLiteOpenHelper(context);
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.delete("DVD", whereClause,whereArgs);
+        db.close();
+    }
+
+
+
+
 }
